3452bb6fd51c89eed6a153d18aa6ec98c3ea8680
Vedant Joshi <vedantjoshi1409@gmail.com>
2025-12-25 01:55:51 -0600

    36 optimised

--- medium/36.java
+++ medium/36.java
@@ -1,32 +1,21 @@
 import java.util.*;
-
 class Solution {
     public boolean isValidSudoku(char[][] board) {
-        HashSet<Character> set;
-        for (int i = 0; i < board.length; i++) {
-            set = new HashSet<>();
-            for (int j = 0; j < board[i].length; j++) {
-                if (board[i][j]=='.') continue;
-                if (set.contains(board[i][j])) return false;
-                set.add(board[i][j]);
-            }
+        HashSet<Character>[] row = new HashSet[9];
+        HashSet<Character>[] col = new HashSet[9];
+        HashSet<Character>[] squares = new HashSet[9];
+        for (int i = 0; i < 9; i++) {
+            row[i] = new HashSet<>();
+            col[i] = new HashSet<>();
+            squares[i] = new HashSet<>();
         }
-        for (int i = 0; i < board[0].length; i++) {
-            set = new HashSet<>();
-            for (int j = 0; j < board.length; j++) {
-                if (board[j][i]=='.') continue;
-                if (set.contains(board[j][i])) return false;
-                set.add(board[j][i]);
-            }
-        }
-        for (int i = 0; i < board.length; i+=3) {
-            for (int j = 0; j < board[i].length; j+=3) {
-                set = new HashSet<>();
-                for(int k = 0; k < 9; k++) {
-                    if (board[i+k/3][j+k%3]=='.') continue;
-                    if (set.contains(board[i+k/3][j+k%3])) return false;
-                    set.add(board[i+k/3][j+k%3]);
-                }
+        for (int i = 0; i < 9; i++) {
+            for(int j = 0; j < 9; j++) {
+                if (board[i][j] == '.') continue;
+                if (row[i].contains(board[i][j]) || col[j].contains(board[i][j]) || squares[i/3*3+j/3%3].contains(board[i][j])) return false;
+                row[i].add(board[i][j]);
+                col[j].add(board[i][j]);
+                squares[i/3*3+j/3%3].add(board[i][j]);
             }
         }
         return true;
