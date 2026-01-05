ae855b2ee9e0fda9688b05c07662e382ff704fa8
Vedant Joshi <vedantjoshi1409@gmail.com>
2026-01-04 20:20:02 -0500

    20

---
+++ easy/20.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+class Solution {
+    public boolean isValid(String s) {
+        Deque<Character> stack = new ArrayDeque<>();
+        for (char c : s.toCharArray()) {
+            if (c == '(') {
+                stack.push(')');
+            } else if (c == '{') {
+                stack.push('}');
+            } else if (c == '[') {
+                stack.push(']');
+            } else if (stack.isEmpty() || stack.peek() != c) {
+                return false;
+            } else {
+                stack.pop();
+            }
+        }
+
+        return stack.isEmpty();
+    }
+}
